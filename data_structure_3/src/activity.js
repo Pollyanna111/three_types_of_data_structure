5faef94f335f97eb1920b45064ea6655f40080b9
Pollyanna <2639436413@qq.com>
2014-08-26 14:16:04 +0800

    activity:Green+Refactor done!

---
+++ data_structure_3/src/activity.js
@@ -0,0 +1,42 @@
+function Activity(name) {
+    this.name = name;
+    this.id = null;
+}
+
+Activity.prototype.create = function () {
+    Activity.save_current_activity(Activity.get_activity_id_generator());
+    Activity.update_activities(this);
+    Activity.save_activity_id_generator();
+};
+
+Activity.update_activities = function(activity){
+    var activities = Activity.get_activities();
+    activity.id = Activity.get_activity_id_generator();
+    activities.push(activity);
+    Activity.save_activities(activities);
+};
+
+Activity.get_activities = function(){
+    return JSON.parse(localStorage.activities) || [];
+};
+
+Activity.save_activities = function(activities){
+    localStorage.activities = JSON.stringify(activities);
+};
+
+Activity.get_current_activity = function(){
+    return JSON.parse(localStorage.current_activity);
+};
+
+Activity.save_current_activity = function(activity_id){
+    localStorage.current_activity = activity_id;
+};
+
+Activity.get_activity_id_generator = function () {
+    return localStorage.activity_id_generator || 0;
+};
+
+Activity.save_activity_id_generator = function(){
+    var activities = Activity.get_activities();
+    localStorage.activity_id_generator = activities.length.toString();
+};
